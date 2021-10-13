c29a2e805116c37dc8c86bcffdf1612db8ed5b42
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-10-12 20:08:57 -0700

    add boolean validations unit tests

---
+++ tests/validators/bool-validations.test.js
@@ -0,0 +1,47 @@
+const booleanValidations = require('../../validations/boolean');
+
+describe('Boolean Validations isBool', () => {
+  test('When value is boolean type, expect validation to be success', async () => {
+    // Arrange
+    const value = true;
+
+    // Act
+    const result = booleanValidations.isBool('test', value);
+
+    // Assert
+    expect(result).toBeTruthy();
+  });
+
+  test('When value is string boolean, expect validation to suceed', async () => {
+    // Arrange
+    const value = 'true';
+
+    // Act
+    const result = booleanValidations.isBool('test', value);
+
+    // Assert
+    expect(result).toBeTruthy();
+  });
+
+  test('When value is array, expect validation to fail', async () => {
+    // Arrange
+    const value = [true];
+
+    // Act
+    const result = booleanValidations.isBool('test', value);
+
+    // Assert
+    expect(result).not.toBeTruthy();
+  });
+
+  test('When value is a number, expect validation to fail', async () => {
+    // Arrange
+    const value = 1;
+
+    // Act
+    const result = booleanValidations.isBool('test', value);
+
+    // Assert
+    expect(result).not.toBeTruthy();
+  });
+});
