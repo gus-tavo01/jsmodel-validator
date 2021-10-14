6954eff8c77d68b52777a3108b468564950433a5
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-10-14 15:38:19 -0700

    fix wrong error message

--- core/validations/string/isString.js
+++ core/validations/string/isString.js
@@ -1,5 +1,5 @@
 module.exports = (key, value, errorMsg = undefined) => {
-  const defaultMsg = `Field '${key}', expected not to be String. Got: ${value}`;
+  const defaultMsg = `Field '${key}', expected to be String. Got: ${value}`;
   const validationFailureMessage = !errorMsg ? defaultMsg : errorMsg;
   return {
     validation: 'isString',
