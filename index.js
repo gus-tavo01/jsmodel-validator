219c8088dc7482f1adb607781f709fb573298c5a
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-11-26 19:57:06 -0800

    add modelValidations into export model

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const validate = require('./core/validate');
 const validateModel = require('./core/validateModel');
 const validations = require('./core/validations');
+const modelValidations = require('./core/movelValidations');
 
 module.exports = {
   validations,
+  modelValidations,
   validate,
   validateModel,
 };
