a00ba694db0c3b467c8ae608de46e08ea8113920
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-11-26 19:34:15 -0800

    enhance validateModel function to support new validation methodology

--- core/validateModel.js
+++ core/validateModel.js
@@ -1,26 +1,44 @@
 const createValidation = require('./createValidation');
 const executeValidations = require('./executeValidations');
 
+function buildValidation(key, value, v) {
+  // handle optional arguments
+  if (v.conf) {
+    return v.validator(key, value, v.conf, v.errorMessage);
+  }
+  // default behavior
+  return v.validator(key, value, v.errorMessage);
+}
+
 module.exports = async (validationModel, entity) => {
   const model = validationModel(entity);
-  const validationKeys = Object.keys(model);
+  const modelKeys = Object.keys(model);
   const entityKeys = Object.keys(entity);
 
+  // check extra fields on entity
   const extraProps = entityKeys
-    .filter((ek) => !validationKeys.find((vk) => vk === ek))
+    .filter((ek) => !modelKeys.find((vk) => vk === ek))
     .map((key) => `Unexpected field '${key}'`);
 
-  const resultsValidations = validationKeys
+  // convert model validations into proccess validations
+  const processValidations = modelKeys
     .map((key) => {
-      const validations = model[key];
-      const optional = validations.find((v) => v.validation === 'isOptional');
+      // create validations from model
+      const keyValidations = model[key].map((v) =>
+        buildValidation(key, entity[key], v)
+      );
+
+      // ignore optional validations when theres no value on them
+      const optional = keyValidations.find(
+        (v) => v.validation === 'isOptional'
+      );
       if (optional && optional.value === undefined) return [];
 
-      return validations.map((v) => createValidation(v));
+      return keyValidations.map((v) => createValidation(v));
     })
     .flat();
 
-  const { isValid, fields } = await executeValidations(resultsValidations);
+  const { isValid, fields } = await executeValidations(processValidations);
   const validationState = {
     isValid: isValid && !(extraProps.length > 0),
     fields: [...fields, ...extraProps],
